3be5874a52f4f752b83087a08dd2efb1c697e397
Hafiz-abdur <abdurrehman2349@gmail.com>
2026-07-13 23:16:10 +0500

    Add expense functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const expenseForm = document.getElementById("expenseForm");
+const titleInput = document.getElementById("title");
+const amountInput = document.getElementById("amount");
+const categoryInput = document.getElementById("category");
+
+let expenses = [];
