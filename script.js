3684720aa884f9319991d7fa4577c7b111e5630c
Hafiz-abdur <abdurrehman2349@gmail.com>
2026-07-13 23:21:20 +0500

    Add expense functionality

--- script.js
+++ script.js
@@ -2,5 +2,25 @@ const expenseForm = document.getElementById("expenseForm");
 const titleInput = document.getElementById("title");
 const amountInput = document.getElementById("amount");
 const categoryInput = document.getElementById("category");
-
 let expenses = [];
+expenseForm.addEventListener("submit", addExpense);
+function addExpense(event) {
+    event.preventDefault();
+    const title = titleInput.value.trim();
+    const amount = Number(amountInput.value);
+    const category = categoryInput.value;
+    if (title === "" || amount <= 0 || category === "") {
+        alert("Please fill all fields correctly.");
+        return;
+    }
+    const expense = {
+        id: Date.now(),
+        title: title,
+        amount: amount,
+        category: category
+    };
+    expenses.push(expense);
+    console.log(expenses);
+    expenseForm.reset();
+    titleInput.focus();    
+}
